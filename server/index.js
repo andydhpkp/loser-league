e7d4bd7530dc617b61d778867f34dcab40c0c43d
Andrew Durham <andrewdurham1094@gmail.com>
2024-08-30 19:45:55 -0600

    updated schedule

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ const fetch = require("node-fetch");
 app.get("/api/proxy/nfl-2023", async (req, res) => {
   try {
     const response = await fetch(
-      "https://pacific-anchorage-21728.herokuapp.com/https://fixturedownload.com/feed/json/nfl-2023",
+      "https://pacific-anchorage-21728.herokuapp.com/https://fixturedownload.com/feed/json/nfl-2024",
       {
         headers: {
           "Content-Type": "application/json",
