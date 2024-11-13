51c7953fd11f01dae006a4956340cc3ea6162cd7
Andrew Durham <andrewdurham1094@gmail.com>
2024-11-12 22:12:43 -0700

    changed to 11

--- public/js/teams.js
+++ public/js/teams.js
@@ -1019,7 +1019,7 @@ async function getCurrentWeek() {
         console.log("This is the current week: " + entry.value);
         //THIS IS WHERE TO CHANGE
         // localStorage.setItem("thisWeek", entry.value.toString());
-        localStorage.setItem("thisWeek", "10");
+        localStorage.setItem("thisWeek", "11");
         return entry.value; // Return the value directly
       }
       console.log("IT GETS TOOOOOO HERERERERERERE");
