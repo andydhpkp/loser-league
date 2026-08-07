1782bd5638820fcb23e6f1fee967eca55a53fbed
Andrew Durham <andrewdurham1094@gmail.com>
2026-08-07 15:49:02 -0600

    Preserve crown artwork proportions

--- test/smoke/crown-assets.spec.js
+++ test/smoke/crown-assets.spec.js
@@ -1,24 +1,33 @@
 const { expect, test } = require("@playwright/test");
 
-test("solo and tied crown artwork fill the shared crown dimensions", async ({
+test("solo and tied crown artwork preserve their aspect ratio in the League table", async ({
   page,
 }) => {
   await page.goto("/index.html");
   await page.setContent(`
+    <link rel="stylesheet" href="/css/styles.css">
     <link rel="stylesheet" href="/css/leagueTable.css">
-    <img class="crown-icon" data-crown="solo" src="/css/assets/crowns/first_time_solo_winner_crown.png">
-    <img class="crown-icon" data-crown="tied" src="/css/assets/crowns/first_time_tie_crown_2_people.png">
+    <style>.table td { padding: 0.5rem; }</style>
+    <table class="table">
+      <tbody>
+        <tr>
+          <td class="crown-column"><img class="crown-icon" data-crown="solo" src="/css/assets/crowns/first_time_solo_winner_crown.png"></td>
+          <td class="crown-column"><img class="crown-icon" data-crown="tied" src="/css/assets/crowns/first_time_tie_crown_2_people.png"></td>
+        </tr>
+      </tbody>
+    </table>
   `);
 
   const crowns = page.locator(".crown-icon");
   await expect(crowns).toHaveCount(2);
-  await expect(crowns.nth(0)).toHaveCSS("width", "30px");
   await expect(crowns.nth(0)).toHaveCSS("height", "23px");
-  await expect(crowns.nth(1)).toHaveCSS("width", "30px");
   await expect(crowns.nth(1)).toHaveCSS("height", "23px");
 
-  const soloAspectRatio = await crowns.nth(0).evaluate(
-    (image) => image.naturalWidth / image.naturalHeight
-  );
-  expect(soloAspectRatio).toBeGreaterThan(1.15);
+  for (const crown of await crowns.all()) {
+    const ratios = await crown.evaluate((image) => ({
+      natural: image.naturalWidth / image.naturalHeight,
+      rendered: image.getBoundingClientRect().width / image.getBoundingClientRect().height,
+    }));
+    expect(ratios.rendered).toBeCloseTo(ratios.natural, 2);
+  }
 });
